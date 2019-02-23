32e04f652ec949b853ed02fa02990df83d0ef67f
douglaskoe <douglasblima01@gmail.com>
2019-02-23 12:52:01 -0300

    Aula 02

---
+++ HandsOn/Aula02/Aula02_introducao_OO_exemplo1.php
@@ -0,0 +1,22 @@
+<?php 
+//Alteração para testar git diff
+//Criando uma classe no Php
+//para definição do nome usado o padrão StudlyCaps
+//Sempre iniciando com letra maiuscula
+
+class Conta
+{
+
+    //Atributos ou propiedade(visibilidade nome)
+    //Para definição do nome usamos padrão camelCase
+    public $saldo;
+    public $titular;
+
+    //Metodo (visibilidade nome)
+    //Metodos podem ou não receber parametros, e podem ou não retornar alguma coisa
+    //Para definição do nome usamos o padrão camelCase
+    public function sacar(){
+        
+    }
+
+}
