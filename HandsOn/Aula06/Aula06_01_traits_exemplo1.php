5bc221ba522567a03c6fc0a95d6d507b9e932b5a
douglaskoe <douglasblima01@gmail.com>
2019-03-16 09:49:17 -0300

    Aula 06

---
+++ HandsOn/Aula06/Aula06_01_traits_exemplo1.php
@@ -0,0 +1,61 @@
+<?php 
+    trait Operacoes
+    {
+        public function depositar($valor)
+        {
+            $this->saldo += $valor;
+        }
+
+        public function sacar($valor)
+        {
+            $this->saldo -= $valor;
+        }
+
+        public function verSaldo()
+        {
+            return $this->saldo;
+        }
+    }
+
+    abstract class Contas
+    {
+        use Operacoes;
+
+        protected $saldo;
+    }
+
+
+    class ContaPoupanca extends Contas
+    {}
+
+    class ContaCorrente extends Contas
+    {}
+
+    $objPoupanca = new ContaPoupanca();
+    $objCorrente = new ContaCorrente();
+
+    $objCorrente->depositar(500);
+    $objPoupanca->depositar(500);
+    
+    echo "<pre>";
+    var_dump($objCorrente);
+    echo "<hr>";
+    var_dump($objPoupanca);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
