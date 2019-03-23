2ceeb8cdf345a107488296e0b37a821d64b65f8e
douglaskoe <douglasblima01@gmail.com>
2019-03-23 17:49:10 -0300

    Aula 10

---
+++ HandsOn/Aula10/Aula10_01_spl_array_access_exemplo1.php
@@ -0,0 +1,56 @@
+<?php
+
+class Usuario implements ArrayAccess
+{
+    public $id;
+    public $nome;
+    public $email;
+    public $senha;
+
+
+    public function offsetExists($offset)
+    {   
+        return isset($this->$offset);
+    }
+
+    public function offsetGet($offset)
+    {
+        if(property_exists($this, $offset)){
+            return $this->$offset;
+        }
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if(property_exists($this, $offset)){
+            $this->$offset = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->$offset);
+    }
+
+}
+
+
+$usuario = new Usuario();
+
+$usuario->id = 10;
+$usuario['id'] = 20;
+$usuario['nome'] = 'Luciana Lima';
+$usuario['email'] = 'luci.lima@gmail.com';
+$usuario['senha'] = '123456';
+
+echo "<pre>";
+print_r($usuario);
+
+echo "<hr>";
+
+foreach($usuario as $atributo => $valor){
+    echo "Atributo: $atributo <br>";
+    echo "Valor : $valor";
+    echo "<hr>";
+}
+
