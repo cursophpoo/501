98c816d364e9bbadf3e04e884226a37be435101b
douglaskoe <douglasblima01@gmail.com>
2019-03-23 12:01:15 -0300

    Aula 08

---
+++ HandsOn/Aula08/MetodoAutoLoad/teste_auto_load.php
@@ -0,0 +1,17 @@
+<?php
+
+function __autoload($classe)
+{
+    echo "<hr> $classe incluido<hr>";
+    require "$classe.php";
+}
+
+
+
+$titular = new Titular();
+$conta = new Contas($titular);
+
+echo "<pre>";
+var_dump($titular);
+echo "<hr>";
+var_dump($conta);
